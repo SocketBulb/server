64aa101e5e285ef50277e418a9e21b6e16abb572
joshpaulchan <joshpaulchan@gmail.com>
2017-08-06 01:02:54 -0400

    Implement email, profile and user models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  User    : require('./user.js'),
+  Email   : require('./email.js'),
+  Profile : require('./profile.js'),
+};
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const db = require('../bin/conndb');
+var model = require('seraph-model');
+
+// define model
+
+const User = model(db, 'user');
+
+// specify timestamp property names
+
+User.useTimestamps('createdAt', 'lastUpdated');
+
+// specify schema
+
+User.schema = {
+  email       : { type: String, required: true },
+  password    : { type: String, required: true }
+}
+
+module.exports = User;
